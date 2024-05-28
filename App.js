7a6d1e0f694502ece939abe53f59424ba0a43714
aproselalbasha <ap.aprose@gmail.com>
2024-05-28 18:14:19 +0530

    updated

---
+++ App.js
@@ -0,0 +1,722 @@
+import react from "react"
+import ReactDOM from "react-dom/client"
+
+const reslist=[ 
+   {
+   "id": "16073",
+   "name": "California Burrito",
+   "cloudinaryImageId": "27238ee4292d42c7b705f2ce8671ff80",
+   "locality": "Malleshwaram",
+   "areaName": "Malleshwaram",
+   "costForTwo": "₹250 for two",
+   "cuisines": [
+     "Mexican",
+     "American",
+     "Salads",
+     "Continental",
+     "Keto",
+     "Healthy Food"
+   ],
+   "avgRating": 4.6,
+   "parentId": "1252",
+   "avgRatingString": "4.6",
+   "totalRatingsString": "10K+",
+   "sla": {
+     "deliveryTime": 46,
+     "lastMileTravel": 5,
+     "serviceability": "SERVICEABLE",
+     "slaString": "45-50 mins",
+     "lastMileTravelString": "5.0 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-28 23:00:00",
+     "opened": true
+   },
+   "badges": {
+     "imageBadges": [
+       {
+         "imageId": "Rxawards/_CATEGORY-Guiltfree.png",
+         "description": "Delivery!"
+       }
+     ]
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         "badgeObject": [
+           {
+             "attributes": {
+               "description": "Delivery!",
+               "imageId": "Rxawards/_CATEGORY-Guiltfree.png"
+             }
+           }
+         ]
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         
+       }
+     }
+   },
+   "loyaltyDiscoverPresentationInfo": {
+     "logoCtx": {
+       "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+     },
+     "freedelMessage": "FREE DELIVERY",
+     "badgeType": "BADGE_TYPE_ONE_LITE"
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+  {
+   "id": "124178",
+   "name": "Starbucks Coffee",
+   "cloudinaryImageId": "2418209798927d733a50f5d2ebcc2aee",
+   "locality": "Lavelle Road",
+   "areaName": "Lavelle Road",
+   "costForTwo": "₹400 for two",
+   "cuisines": [
+     "Beverages",
+     "Cafe",
+     "Snacks",
+     "Desserts",
+     "Bakery",
+     "Ice Cream"
+   ],
+   "avgRating": 4.4,
+   "parentId": "195515",
+   "avgRatingString": "4.4",
+   "totalRatingsString": "1K+",
+   "sla": {
+     "deliveryTime": 20,
+     "lastMileTravel": 0.5,
+     "serviceability": "SERVICEABLE",
+     "slaString": "20-25 mins",
+     "lastMileTravelString": "0.5 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-28 23:59:00",
+     "opened": true
+   },
+   "badges": {
+     "textExtendedBadges": [
+       {
+         "iconId": "guiltfree/GF_Logo_android_3x",
+         "shortDescription": "options available",
+         "fontColor": "#7E808C"
+       }
+     ]
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         "badgeObject": [
+           {
+             "attributes": {
+               "description": "",
+               "fontColor": "#7E808C",
+               "iconId": "guiltfree/GF_Logo_android_3x",
+               "shortDescription": "options available"
+             }
+           }
+         ]
+       }
+     }
+   },
+   "aggregatedDiscountInfoV3": {
+     "header": "₹110 OFF",
+     "subHeader": "ABOVE ₹199",
+     "discountTag": "FLAT DEAL"
+   },
+   "loyaltyDiscoverPresentationInfo": {
+     "logoCtx": {
+       "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+     },
+     "freedelMessage": "FREE DELIVERY",
+     "badgeType": "BADGE_TYPE_ONE_LITE"
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+ {
+   "id": "570511",
+   "name": "Bakingo",
+   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/24/a96b7994-f501-41ce-961d-801e3198cde1_570511.JPG",
+   "locality": "Vasanth Nagar",
+   "areaName": "6th Main Road",
+   "costForTwo": "₹299 for two",
+   "cuisines": [
+     "Bakery",
+     "Desserts",
+     "Beverages",
+     "Snacks"
+   ],
+   "avgRating": 4.6,
+   "parentId": "3818",
+   "avgRatingString": "4.6",
+   "totalRatingsString": "1K+",
+   "sla": {
+     "deliveryTime": 27,
+     "lastMileTravel": 3,
+     "serviceability": "SERVICEABLE",
+     "slaString": "25-30 mins",
+     "lastMileTravelString": "3.0 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-29 01:00:00",
+     "opened": true
+   },
+   "badges": {
+     "textExtendedBadges": [
+       {
+         "iconId": "v1705582451/Ratnesh_Badges/Perfect_cake.png",
+         "shortDescription": "Perfect cake delivery",
+         "fontColor": "#7E808C"
+       }
+     ]
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         "badgeObject": [
+           {
+             "attributes": {
+               "description": "",
+               "fontColor": "#7E808C",
+               "iconId": "v1705582451/Ratnesh_Badges/Perfect_cake.png",
+               "shortDescription": "Perfect cake delivery"
+             }
+           }
+         ]
+       }
+     }
+   },
+   "aggregatedDiscountInfoV3": {
+     "header": "₹110 OFF",
+     "subHeader": "ABOVE ₹199",
+     "discountTag": "FLAT DEAL",
+     "discountCalloutInfo": {
+       "message": "Free Delivery",
+       "logoCtx": {
+         "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+       }
+     }
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+  {
+   "id": "23847",
+   "name": "Domino's Pizza",
+   "cloudinaryImageId": "d0450ce1a6ba19ea60cd724471ed54a8",
+   "locality": "Rest House Road",
+   "areaName": "Brigade Road",
+   "costForTwo": "₹400 for two",
+   "cuisines": [
+     "Pizzas",
+     "Italian",
+     "Pastas",
+     "Desserts"
+   ],
+   "avgRating": 4.4,
+   "parentId": "2456",
+   "avgRatingString": "4.4",
+   "totalRatingsString": "5K+",
+   "sla": {
+     "deliveryTime": 25,
+     "lastMileTravel": 1.7,
+     "serviceability": "SERVICEABLE",
+     "slaString": "20-25 mins",
+     "lastMileTravelString": "1.7 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-28 22:59:00",
+     "opened": true
+   },
+   "badges": {
+     "imageBadges": [
+       {
+         "imageId": "Rxawards/_CATEGORY-Pizza.png",
+         "description": "Delivery!"
+       }
+     ]
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         "badgeObject": [
+           {
+             "attributes": {
+               "description": "Delivery!",
+               "imageId": "Rxawards/_CATEGORY-Pizza.png"
+             }
+           }
+         ]
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         
+       }
+     }
+   },
+   "aggregatedDiscountInfoV3": {
+     "header": "₹110 OFF",
+     "subHeader": "ABOVE ₹199",
+     "discountTag": "FLAT DEAL"
+   },
+   "loyaltyDiscoverPresentationInfo": {
+     "logoCtx": {
+       "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+     },
+     "freedelMessage": "FREE DELIVERY",
+     "badgeType": "BADGE_TYPE_ONE_LITE"
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+  {
+   "id": "10575",
+   "name": "Pizza Hut",
+   "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+   "locality": "Shanti Nagar",
+   "areaName": "Shanti Nagar",
+   "costForTwo": "₹600 for two",
+   "cuisines": [
+     "Pizzas"
+   ],
+   "avgRating": 4.2,
+   "parentId": "721",
+   "avgRatingString": "4.2",
+   "totalRatingsString": "5K+",
+   "sla": {
+     "deliveryTime": 35,
+     "lastMileTravel": 2.7,
+     "serviceability": "SERVICEABLE",
+     "slaString": "30-35 mins",
+     "lastMileTravelString": "2.7 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-29 01:00:00",
+     "opened": true
+   },
+   "badges": {
+     
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         
+       }
+     }
+   },
+   "aggregatedDiscountInfoV3": {
+     "header": "20% OFF",
+     "subHeader": "UPTO ₹50"
+   },
+   "loyaltyDiscoverPresentationInfo": {
+     "logoCtx": {
+       "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+     },
+     "freedelMessage": "FREE DELIVERY",
+     "badgeType": "BADGE_TYPE_ONE_LITE"
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+ {
+   "id": "3241",
+   "name": "Meghana Foods",
+   "cloudinaryImageId": "e0vvulfbahjxjz6k4uwi",
+   "locality": "Residency Road",
+   "areaName": "Residency Road",
+   "costForTwo": "₹500 for two",
+   "cuisines": [
+     "Biryani",
+     "Andhra",
+     "South Indian",
+     "Chinese",
+     "Seafood"
+   ],
+   "avgRating": 4.6,
+   "parentId": "635",
+   "avgRatingString": "4.6",
+   "totalRatingsString": "10K+",
+   "sla": {
+     "deliveryTime": 22,
+     "lastMileTravel": 2.2,
+     "serviceability": "SERVICEABLE",
+     "slaString": "20-25 mins",
+     "lastMileTravelString": "2.2 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-28 23:59:00",
+     "opened": true
+   },
+   "badges": {
+     "imageBadges": [
+       {
+         "imageId": "Rxawards/_CATEGORY-Biryani.png",
+         "description": "Delivery!"
+       }
+     ]
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         "badgeObject": [
+           {
+             "attributes": {
+               "description": "Delivery!",
+               "imageId": "Rxawards/_CATEGORY-Biryani.png"
+             }
+           }
+         ]
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         
+       }
+     }
+   },
+   "loyaltyDiscoverPresentationInfo": {
+     "logoCtx": {
+       "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+     },
+     "freedelMessage": "FREE DELIVERY",
+     "badgeType": "BADGE_TYPE_ONE_LITE"
+   },
+   "orderabilityCommunication": {
+     "title": {
+       
+     },
+     "subTitle": {
+       
+     },
+     "message": {
+       
+     },
+     "customIcon": {
+       
+     }
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+  {
+   "id": "464509",
+   "name": "Leon's - Burgers & Wings (Leon Grill)",
+   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/2/4c1e29e2-fecf-41b6-8b21-5a58338247fe_464509.jpg",
+   "locality": "Frazer Town",
+   "areaName": "Frazer Town",
+   "costForTwo": "₹300 for two",
+   "cuisines": [
+     "American",
+     "Snacks",
+     "Turkish",
+     "Portuguese",
+     "Continental"
+   ],
+   "avgRating": 4.5,
+   "parentId": "371281",
+   "avgRatingString": "4.5",
+   "totalRatingsString": "5K+",
+   "sla": {
+     "deliveryTime": 30,
+     "lastMileTravel": 4.2,
+     "serviceability": "SERVICEABLE",
+     "slaString": "30-35 mins",
+     "lastMileTravelString": "4.2 km",
+     "iconType": "ICON_TYPE_EMPTY"
+   },
+   "availability": {
+     "nextCloseTime": "2024-05-29 03:00:00",
+     "opened": true
+   },
+   "badges": {
+     
+   },
+   "isOpen": true,
+   "type": "F",
+   "badgesV2": {
+     "entityBadges": {
+       "imageBased": {
+         
+       },
+       "textBased": {
+         
+       },
+       "textExtendedBadges": {
+         
+       }
+     }
+   },
+   "aggregatedDiscountInfoV3": {
+     "header": "₹125 OFF",
+     "subHeader": "ABOVE ₹199",
+     "discountTag": "FLAT DEAL"
+   },
+   "loyaltyDiscoverPresentationInfo": {
+     "logoCtx": {
+       "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+     },
+     "freedelMessage": "FREE DELIVERY",
+     "badgeType": "BADGE_TYPE_ONE_LITE"
+   },
+   "orderabilityCommunication": {
+     "title": {
+       
+     },
+     "subTitle": {
+       
+     },
+     "message": {
+       
+     },
+     "customIcon": {
+       
+     }
+   },
+   "differentiatedUi": {
+     "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+     "differentiatedUiMediaDetails": {
+       "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+       "lottie": {
+         
+       },
+       "video": {
+         
+       }
+     }
+   },
+   "reviewsSummary": {
+     
+   },
+   "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+   "restaurantOfferPresentationInfo": {
+     
+   }
+ },
+
+ 
+ 
+]
+const Header=()=>{
+   return(
+   <div className="header">
+      <div className="logo">
+         <img src="https://i.pinimg.com/originals/02/fd/5c/02fd5cc15841730936c0c5c555dae3a9.png" alt="" />
+      </div>
+      <div className="nav-item">
+         <ul>
+            <li>HOME</li>
+            <li>ABOUT US</li>
+            <li>CONTACT US</li>
+            <li>KART</li>
+         </ul>
+      </div>
+
+   </div>
+   )
+}
+const Restrocard=(resitems)=>{
+   const {name,cuisines,costForTwo,avgRating,locality,cloudinaryImageId}=resitems.resitems
+   
+   return (
+   <div className="restro-card">
+      <div className="food-image">
+         <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId }alt="res-card" />
+      </div>
+      <div className="menu-items">
+         <h3>{name}</h3>
+         <p className="cuisines">{cuisines.join(",")}</p>
+         <p>{avgRating} ⭐⭐⭐</p>
+         <p>{costForTwo}</p>
+         <p>{locality}</p>
+
+      </div>
+     
+
+   </div>
+)}
+const Body=()=>{
+   return(<div className="body">
+   <div className="search">search</div>
+   <div className="resto-menu">
+    {reslist.map((restrant)=>{
+      return (
+   <Restrocard resitems={restrant} key={restrant.id}/>)
+
+    })} 
+    
+      
+
+
+   </div>
+   </div>
+   )
+}
+const Footer=()=>{
+   return (<div className="footer">
+      <p> ©️ All rights resvered By ❤️Ayaan</p>
+
+   </div>)
+
+}
+const Applayout=()=>{
+   return(
+      <div>
+   <Header/>
+   <Body/>
+   <Footer/>
+   </div>
+
+)
+
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Applayout/>)
