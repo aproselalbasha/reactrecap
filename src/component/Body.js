72642fb7825382dccf639a00a5c611f930aea6d5
aproselalbasha <ap.aprose@gmail.com>
2024-05-30 23:14:23 +0530

    updated

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,10 +1,21 @@
 import Restrocard from "./Restrocard"
 import reslist from "../utlis/mockdata"
+import {useState} from "react"
 const Body=()=>{
+   const [resrant,setresrant]=useState(reslist)
+ 
+
  return(<div className="body">
-    <div className="search">search</div>
+    <div className="search">
+      <button id="filter_btn" onClick={()=>{
+       var filterlist=resrant.filter((res)=>{ return res.avgRating>4.5})
+       setresrant(filterlist)
+      }
+      }>Filter AVG 4.5</button></div>
+      
+      
     <div className="resto-menu">
-     {reslist.map((restrant)=>{
+     {resrant.map((restrant)=>{
        return (
     <Restrocard resitems={restrant} key={restrant.id}/>)
  
